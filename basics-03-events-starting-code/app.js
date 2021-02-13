cef2172c1e681b5f8a1c484da11d3e33ed8d66cd
Unlimited23 <petyoruzhin@gmail.com>
2021-02-13 18:55:29 +0200

    event modifiers

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -1,9 +1,27 @@
 const app = Vue.createApp({
   data() {
+    const name = 'Petyo'
     return {
       counter: 0,
+      num: 5,
+      name: name,
+      confirmedName: name,
     };
   },
+  methods: {
+    add(num) {
+     this.counter += this.num; 
+    },
+    reduce() {
+      this.counter -= this.num;
+    },
+    updateName(e, name) {
+      this.name = e.target.value || name;
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    }
+  }
 });
 
-app.mount('#events');
+const mountedApp = app.mount('#events');
