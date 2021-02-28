83affa00ebc9958a599f90327c228d2769247a16
Unlimited23 <petyoruzhin@gmail.com>
2021-02-28 21:58:58 +0200

    two-way databinding

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -5,8 +5,18 @@ const app = Vue.createApp({
       name: ''
     };
   },
+  computed: {
+    fullName() {
+      // This will be called only when a modification in the name property is made;
+      return this.name + ' ' + 'Ruzhin';
+    }
+  },
   methods: {
-    setName(event, lastName) {
+    outputFullName() {
+      // This will be called every time any modification is made inside the app;
+      return this.name + ' ' + 'Ruzhin';
+    },
+    setName(event) {
       this.name = event.target.value + ' ' + lastName;
     },
     add(num) {
@@ -15,8 +25,11 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
+    },
+    resetInput() {
+      this.name = '';
     }
   }
 });
 
-app.mount('#events');
+const mountedApp = app.mount('#events');
